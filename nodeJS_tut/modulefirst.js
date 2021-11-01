bbdd7c8a8ca9ad3879de76467c91a75a3366e1e9
BATspock <30798316+BATspock@users.noreply.github.com>
2021-11-01 13:33:31 +0530

    exporting

--- nodeJS_tut/modulefirst.js
+++ nodeJS_tut/modulefirst.js
@@ -1,3 +1,9 @@
-const simple = require('./modulesecond')
+const simple = require('./modulesecond');
+const subjects = require('./sub.js');
 
-simple()
+let a = Object.keys(subjects);
+console.log(a[0])
+for (let i = 0; i < a.length; i++) {
+    console.log(a[i]);
+}
+simple();
