68c29039ce46378ea2272a4e27092e6b755facd0
BATspock <30798316+BATspock@users.noreply.github.com>
2021-11-02 19:03:07 +0530

    working with JSON

---
+++ nodeJS_tut/hindi.js
@@ -0,0 +1,8 @@
+const Hindi= {
+    vyakaran: "Vyakaran meaning in English is GRAMMAR. Vyakaran ka matlab english me GRAMMAR hai. Get meaning and translation of Vyakaran in English language with grammar, synonyms and antonyms", 
+    varnamala: "The Sanskrit word ‘Varnmala’ is made up of 2 different words. Varna and Mala. Varna means the smallest piece of a language", 
+    sandhi: "the process whereby the form of a word changes as a result of its position in an utterance"
+        
+    };
+    
+module.exports = Hindi;
--- nodeJS_tut/modulefirst.js
+++ nodeJS_tut/modulefirst.js
@@ -1,15 +1,20 @@
 const simple = require('./modulesecond');
 const subjects = require('./sub.js');
+const Hindi = require('./hindi.js');
 
 let a = Object.keys(subjects);
-console.log(a[0])
-for (let i = 0; i < a.length; i++) {
-    console.log(a[i]);
-}
+//console.log(a[0])
+//for (let i = 0; i < a.length; i++) {
+//    console.log(a[i]);
+//}
 
 let sub = "English";
-let subtop = "Grammar";
-console.log(subjects[sub][subtop]);
+console.log(Object.keys(Hindi))
+let subtop = "sandhi";
+console.log(Hindi[subtop]);
 
 
-simple();
+//simple();
+const json = require('./example.json');
+
+console.log(json);
