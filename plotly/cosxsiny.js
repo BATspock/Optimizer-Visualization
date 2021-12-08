730b68053a11347a811ad189686500f53947bcd5
BATspock <aditya20kishore@gmail.com>
2021-12-08 22:25:06 +0530

    created graph for trignometric multiplication function

---
+++ plotly/cosxsiny.js
@@ -0,0 +1,42 @@
+function trignometric() {
+    var arr = [];
+    for (let i = 0; i < 500; i++) {
+        var temp = []
+        for (let j = 0;j<500;j++){
+            temp.push(Math.cos( i * Math.PI / 180)* Math.sin(j * Math.PI /180))
+        }
+        arr.push(temp);
+    }
+    console.log(arr.length);
+    return arr;
+}
+
+var data = trignometric();
+//console.log(data);
+Plotly.newPlot('chart', [{
+    z: data,
+  type: 'surface',
+  contours: {
+    z: {
+      show:true,
+      usecolormap: true,
+      highlightcolor:"#42f462",
+      project:{z: true}
+    }
+  }
+}], layout);
+
+var layout = {
+    title: 'Mt Bruno Elevation With Projected Contours',
+    scene: {camera: {eye: {x: 1.87, y: 0.88, z: -0.64}}},
+    autosize: false,
+    width: 1000,
+    height: 1000,
+    margin: {
+      l: 65,
+      r: 50,
+      b: 65,
+      t: 90,
+    }
+  };
+
