a58809736587b4a5fd77a15e85dd73b1c75ec90f
BATspock <aditya20kishore@gmail.com>
2021-12-19 11:36:16 +0530

    exponent plots

---
+++ plotly/exponent.js
@@ -0,0 +1,36 @@
+var zPts = []; 
+var xPts = [];
+var yPts = [];
+
+
+for(x=-5; x<5; x+=0.1) {
+  let zTemp = [];
+  let yTemp = [];
+  let xTemp = [];
+  for (y=-5; y<5; y+=0.1) {
+    //zTemp.push(Math.sin(x)*Math.cos(y));
+    zTemp.push(x*Math.exp(-1*Math.pow(x,2) - Math.pow(y,2)));
+    yTemp.push(y);
+    xTemp.push(x);
+  }
+  zPts.push(zTemp);
+  yPts.push(yTemp);
+  xPts.push(xTemp);
+}
+
+var function_plot = {
+    z: zPts,
+    x: xPts,
+    y: yPts,
+    
+    type: 'surface',
+    
+    contours: {
+        z: {
+          show:true,
+          usecolormap: true,
+          highlightcolor:"#42f462",
+          project:{z: true}
+        }
+      }
+};
