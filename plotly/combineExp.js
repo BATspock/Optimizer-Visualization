a8976d61701ce19cd3ba29b660f84d52a75b9ed0
BATspock <aditya20kishore@gmail.com>
2021-12-24 18:53:46 -0800

    combined EXP plots

---
+++ plotly/combineExp.js
@@ -0,0 +1,107 @@
+var zPts = []; 
+var xPts = [];
+var yPts = [];
+
+
+for(x=-5; x<5; x+=0.1) {
+  let zTemp = [];
+  let yTemp = [];
+  let xTemp = [];
+  for (y=-5; y<5; y+=0.1) {
+    //zTemp.push(Math.sin(x)*Math.cos(y));
+    zTemp.push(x*Math.exp(-1*Math.pow(x,2) - Math.pow(y,2)));
+    yTemp.push(y);
+    xTemp.push(x);
+  }
+  zPts.push(zTemp);
+  yPts.push(yTemp);
+  xPts.push(xTemp);
+}
+
+
+var x_val = [];
+var y_val = [];
+var z_val = [];
+var c = [];
+
+
+var x_val = [];
+var y_val = [];
+var z_val = [];
+var c = [];
+
+function loss_function(x, y) {
+    return x*Math.exp(-1*Math.pow(x,2) - Math.pow(y,2));
+}
+
+let X = -1.4;
+let Y = 0.3;
+
+x_val.push(X);
+y_val.push(Y);
+z_val.push(loss_function(X, Y));
+
+let learning_rate  = 0.001;
+
+for (let i = 0;i < 100000;i++) {
+    X = X - (learning_rate*(-2*X+1)*loss_function(X,Y));
+    Y = Y - (learning_rate*(-2*Y)*loss_function(X,Y));
+    x_val.push(X);
+    y_val.push(Y);
+    z_val.push(loss_function(X, Y));
+    c.push(i*0.001);
+}
+
+
+var gradient_plot = {
+    type: 'scatter3d',
+    mode: 'lines+markers',
+    
+    x: x_val,
+    y: y_val,
+    z: z_val,
+    
+    line: {
+      width: 6,
+      color: c,
+      colorscale: "Viridis"},
+    
+    marker: {
+      size: 3.5,
+      color: c,
+      colorscale: "Greens",
+      cmin: -20,
+      cmax: 50
+    }
+};
+
+
+var function_plot = {
+    z: zPts,
+    x: xPts,
+    y: yPts,
+    
+    type: 'surface',
+    
+    contours: {
+        z: {
+          show:true,
+          usecolormap: true,
+          highlightcolor:"#42f462",
+          project:{z: true}
+        }
+      }
+};
+
+var plotData = [gradient_plot, function_plot];
+
+var layout = {
+    title: 'Combined plot Exp',
+    height: 700,
+    width: 700
+    //yaxis: {title: 'Simple Contour Plot Axis', range: [-20, 20]},
+    //yaxis2: {title: 'Line and Scatter Plot Axis', range: [-20, 20]}
+};
+
+Plotly.newPlot('plotDiv', plotData, layout);
+
