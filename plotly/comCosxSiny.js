388d06600342103a439647a94963f39fad9810df
BATspock <aditya20kishore@gmail.com>
2022-01-05 22:27:31 -0800

    added scaling to graphs

--- plotly/comCosxSiny.js
+++ plotly/comCosxSiny.js
@@ -28,8 +28,8 @@ function loss_function(x, y) {
     return Math.sin(Math.sqrt(1+Math.cos(x)*Math.sin(y)));
 }
 
-let X = 5.9;
-let Y = 8.3;
+let X = 0.5;
+let Y = 1.2;
 
 x_val.push(X);
 y_val.push(Y);
@@ -91,9 +91,28 @@ var function_plot = {
 var plotData = [gradient_plot, function_plot];
 
 var layout = {
-    title: 'Combined plot Exp',
-    height: 700,
-    width: 1000
+    title: 'Combined CosX SinY',
+    height: 800,
+    width: 1500,
+
+    scene: {
+      xaxis: { title: 'X' },
+      yaxis: { title: 'Y' },
+      zaxis: { title: 'Z' },
+      aspectratio: {
+        x: 1.2,
+        y: 1.2,
+        z: 0.75
+      }
+    },
+    autosize: true,
+    margin: {
+      l: 50,
+      r: 0,
+      b: 0,
+      t: 30,
+      pad: 0
+    },
     //yaxis: {title: 'Simple Contour Plot Axis', range: [-20, 20]},
     //yaxis2: {title: 'Line and Scatter Plot Axis', range: [-20, 20]}
 };
