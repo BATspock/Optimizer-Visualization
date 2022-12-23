ea0d227e21f171e2a8496714f1458028542eaf5d
Aditya Kishore <aditya20kishore@gmail.com>
2022-12-23 20:30:38 +0530

    added tabs

--- deploysite/src/app/app.module.ts
+++ deploysite/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PolynomialComponent } from './polynomial/polynomial.component';
 import { ExponentialComponent } from './exponential/exponential.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     PolynomialComponent,
-    ExponentialComponent
+    ExponentialComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
