369e51a5211d869fd63e300d337fb03c56eba9b6
Aditya Kishore <aditya20kishore@gmail.com>
2022-12-23 19:25:32 +0530

    created polynomial and exp components

--- deploysite/src/app/app.module.ts
+++ deploysite/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PolynomialComponent } from './polynomial/polynomial.component';
+import { ExponentialComponent } from './exponential/exponential.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolynomialComponent,
+    ExponentialComponent
   ],
   imports: [
     BrowserModule,
---
+++ deploysite/src/app/exponential/exponential.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exponential',
+  templateUrl: './exponential.component.html',
+  styleUrls: ['./exponential.component.scss']
+})
+export class ExponentialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
